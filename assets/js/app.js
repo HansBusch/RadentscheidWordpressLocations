780f10cea52b8d57ba4a5350f3a47617efa68a77
Hans Busch <47081128+HansBusch@users.noreply.github.com>
2022-08-20 09:49:01 +0200

    Fix initial popup width with view text.

--- assets/js/app.js
+++ assets/js/app.js
@@ -121,7 +121,7 @@ spReady(function() {
             // Add the first image to bubble
             if(locations[key]['images'] != undefined) {
               if(locations[key]['images'][0] != undefined) {
-                bubbleHtml+= '<a href="'+locations[key]['url']+'"><img class="" src='+locations[key]['images'][0]['thumbnails'][300]+'></a><br>';
+                bubbleHtml+= '<a href="'+locations[key]['url']+'"><img class="" width="300" src='+locations[key]['images'][0]['thumbnails'][300]+'></a><br>';
               }
             }
 			var descr = locations[key]['description'];
